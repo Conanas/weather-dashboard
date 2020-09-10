2b9c95b3e248347974b8d8e267a34959c2b3bbdc
Conanas <tmhaigh@gmail.com>
2020-09-11 09:23:47 +1000

    added comments for plan of action

---
+++ script.js
@@ -0,0 +1,8 @@
+var apiKey = "accd5d59f380f6347ac44664ded3ecef";
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}
+// api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={your api key}
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
