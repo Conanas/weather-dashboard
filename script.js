b1e9a5c1fedc60a286315c5a317e5df6ad38c0f9
Conanas <tmhaigh@gmail.com>
2020-09-11 14:26:36 +1000

    getting response from api

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 var apiKey = "accd5d59f380f6347ac44664ded3ecef";
-
+var queryURL = "";
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 // api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}
 // api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={your api key}
 
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
+
+var searchInput = $("#search-input");
+var searchButton = $("#search-button");
+
+searchButton.on("click", searchCity);
+
+function searchCity(event) {
+    event.preventDefault();
+    getData();
+}
+
+function getData() {
+    createURL();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(processData)
+}
+
+function createURL() {
+    var cityName = searchInput.val();
+    queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+}
+
+function processData(response) {}
